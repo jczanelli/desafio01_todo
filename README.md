76322162a0d50e9cde5dfd41494eb36857ab1d59
Juliano Cavinato Zanelli <julianozanelli@gmail.com>
2023-01-04 20:50:56 -0300

    Finished

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 Nesse desafio, você deverá criar uma aplicação para treinar o que aprendeu até agora no Node.js!
 
 Essa será uma aplicação para gerenciar tarefas (em inglês todos). Será permitida a criação de um usuário com name e username, bem como fazer o CRUD de todos:
-[] Criar um novo todo;
-[] Listar todos os todos;
-[] Alterar o title e deadline de um todo existente;
-[] Marcar um todo como feito;
-[] Excluir um todo;
+[X] Criar um novo todo;
+[X] Listar todos os todos;
+[X] Alterar o title e deadline de um todo existente;
+[X] Marcar um todo como feito;
+[X] Excluir um todo;
 
 Tudo isso para cada usuário em específico (o username será passado pelo header)
--- src/index.js
+++ src/index.js
@@ -10,45 +10,107 @@ app.use(express.json());
 
 const users = [];
 
+//Middleware that checks if user exists
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const user = users.find(user => user.username === username);
+  if(!user){
+    return response.status(404).json({error: "User not found"});
+  };
+  request.user = user;
+  return next();
 }
 
+//Create new user account
 app.post('/users', (request, response) => {
   const {name, username} = request.body;
+
   const userAlreadyExists = users.some((user) => user.username === username);
 
   if(userAlreadyExists){
     return response.status(400).json({error: "User already exists"});
   };
 
-  users.push({
-    name,
-    username,
+  const user = ({
     id: uuidv4(),
-    todos:[],
+    name,
+    username,    
+    todos: []
   });
+
+  users.push(user);
+  return response.status(201).json(user);
 });
 
+//List all to dos
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  const {username} = request;
-  return response.json(username.todos);
+  const {user} = request;
+
+  return response.json(user.todos);
 });
 
+//Create new to do
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const createNewTodo = { 
+    id: uuidv4(),
+    title,
+    done: false, 
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(createNewTodo);
+  return response.status(201).json(createNewTodo);
 });
 
+//Update to do
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request;
+  const { title, deadline } = request.body;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo){
+    return response.status(404).json({error: "To do not found"});
+  }
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.json(todo);
 });
 
+//Change to do to done
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find(todo => todo.id === id);
+  if(!todo){
+    return response.status(404).json({error: "To do not found"});
+  }
+  
+  todo.done = true;
+  return response.json(todo);
 });
 
+//Delete to do
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todoIndex = user.todos.findIndex(todo => todo.id === id);
+
+  if(todoIndex === -1){
+    return response.status(404).json({error: "To do not found"});
+  }
+
+  user.todos.splice(todoIndex, 1)
+  return response.status(204).send();
 });
 
 module.exports = app;
